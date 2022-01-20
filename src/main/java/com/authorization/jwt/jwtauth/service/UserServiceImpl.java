7a8c51412ea0ee95287071ea59798f66ff03975d
Ricardo Serrano <ricardo.ss@live.com>
2022-01-20 15:07:06 -0600

    feat: Add inyection of repos

--- src/main/java/com/authorization/jwt/jwtauth/service/UserServiceImpl.java
+++ src/main/java/com/authorization/jwt/jwtauth/service/UserServiceImpl.java
@@ -3,7 +3,10 @@ package com.authorization.jwt.jwtauth.service;
 import java.util.List;
 import com.authorization.jwt.jwtauth.domain.Role;
 import com.authorization.jwt.jwtauth.domain.User;
+import com.authorization.jwt.jwtauth.repository.RoleRepository;
+import com.authorization.jwt.jwtauth.repository.UserRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,6 +16,15 @@ import lombok.extern.slf4j.Slf4j;
 @Transactional
 @Slf4j
 public class UserServiceImpl implements UserService{
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository){
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+    }
 
     @Override
     public User saveUser(User user) {
