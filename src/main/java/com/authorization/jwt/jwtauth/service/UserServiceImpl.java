37ca178ae12b2a0bde81380aeeb5db97ac453f82
Ricardo Serrano <ricardo.ss@live.com>
2022-01-21 12:52:23 -0600

    fix: Remove innecesary dependencies

--- src/main/java/com/authorization/jwt/jwtauth/service/UserServiceImpl.java
+++ src/main/java/com/authorization/jwt/jwtauth/service/UserServiceImpl.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-import javax.persistence.metamodel.CollectionAttribute;
-
 import com.authorization.jwt.jwtauth.domain.Role;
 import com.authorization.jwt.jwtauth.domain.User;
 import com.authorization.jwt.jwtauth.repository.RoleRepository;
