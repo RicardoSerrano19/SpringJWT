a056e64a7306c8d035f9d788218bad685cd02b00
Ricardo Serrano <ricardo.ss@live.com>
2022-01-21 11:57:28 -0600

    feat: Add role repository

--- src/main/java/com/authorization/jwt/jwtauth/repository/RoleRepository.java
+++ src/main/java/com/authorization/jwt/jwtauth/repository/RoleRepository.java
@@ -2,7 +2,9 @@ package com.authorization.jwt.jwtauth.repository;
 
 import com.authorization.jwt.jwtauth.domain.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long>{
     Role findByName(String name);
 }
