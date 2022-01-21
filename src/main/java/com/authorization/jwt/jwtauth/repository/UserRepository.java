bd423d2fd3050d62f7734e34653684079ddd73f4
Ricardo Serrano <ricardo.ss@live.com>
2022-01-21 11:57:39 -0600

    feat: Add user repository

--- src/main/java/com/authorization/jwt/jwtauth/repository/UserRepository.java
+++ src/main/java/com/authorization/jwt/jwtauth/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.authorization.jwt.jwtauth.repository;
 
 import com.authorization.jwt.jwtauth.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
     User findByUsername(String username);
 }
