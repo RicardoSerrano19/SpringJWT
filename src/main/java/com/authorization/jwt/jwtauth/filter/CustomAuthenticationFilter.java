f276fe1fbb86f2d131a5d2ebe4470397f37e9315
Ricardo Serrano <ricardo.ss@live.com>
2022-01-21 20:03:43 -0600

    feat: Add custom filter

--- src/main/java/com/authorization/jwt/jwtauth/filter/CustomAuthenticationFilter.java
+++ src/main/java/com/authorization/jwt/jwtauth/filter/CustomAuthenticationFilter.java
@@ -1,17 +1,24 @@
 package com.authorization.jwt.jwtauth.filter;
 
 import java.io.IOException;
+import java.util.Date;
+import java.util.stream.Collectors;
+import java.util.List;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import lombok.extern.slf4j.Slf4j;
@@ -39,7 +46,34 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
             Authentication authResult) throws IOException, ServletException {
-        super.successfulAuthentication(request, response, chain, authResult);
+        User user = (User)authResult.getPrincipal();
+        Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+        String accessToken = generateJTWToken(user, 12000, request.getRequestURL().toString(), "roles", algorithm);
+        String refreshToken = generateJTWToken(user, 24000, request.getRequestURL().toString(), algorithm);
+        response.setHeader("access_token", accessToken);
+        response.setHeader("refresh_token", refreshToken);
+    }
+
+    private String generateJTWToken(User user, int milliseconds, String issuer, String claim, Algorithm algorithm){
+        return JWT.create()
+                .withSubject(user.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + milliseconds))
+                .withIssuer(issuer)
+                .withClaim(claim, getClaimValues(user))
+                .sign(algorithm);
+    }
+
+    private String generateJTWToken(User user, int milliseconds, String issuer, Algorithm algorithm){
+        return JWT.create()
+                .withSubject(user.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + milliseconds))
+                .withIssuer(issuer)
+                .sign(algorithm);
     }
     
+    private List<String> getClaimValues(User user){
+        return user.getAuthorities().stream()
+            .map(role -> role.getAuthority())
+            .collect(Collectors.toList());
+    }
 }
