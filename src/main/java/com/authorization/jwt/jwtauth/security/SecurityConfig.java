e8cb509ba45f2acab1590858157777adc0126c2d
Ricardo Serrano <ricardo.ss@live.com>
2022-01-24 14:43:11 -0600

    feat: Add endpoint matcher for each rol

--- src/main/java/com/authorization/jwt/jwtauth/security/SecurityConfig.java
+++ src/main/java/com/authorization/jwt/jwtauth/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.authorization.jwt.jwtauth.security;
 import com.authorization.jwt.jwtauth.filter.CustomAuthenticationFilter;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -31,7 +32,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.authorizeRequests().anyRequest().permitAll();
+        http.authorizeRequests().antMatchers("/login").permitAll();
+        http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/v1/users").permitAll();
+        http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/v1/users/{username}").hasAnyAuthority("ROLE_USER");
+        http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/v1/users").hasAnyAuthority("ROLE_ADMIN");
+        http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/v1/roles").hasAnyAuthority("ROLE_ADMIN");
+        http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/v1/users/roles").hasAnyAuthority("ROLE_ADMIN");
+        http.authorizeRequests().anyRequest().authenticated();
         http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
     }
 
