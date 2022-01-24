1b4ccd8e227af95294ade5e27753216444c1fcc9
Ricardo Serrano <ricardo.ss@live.com>
2022-01-24 12:15:08 -0600

    feat: Return access_token and refresh_token and send message when unsuccessfulAuthentication

--- src/main/java/com/authorization/jwt/jwtauth/filter/CustomAuthenticationFilter.java
+++ src/main/java/com/authorization/jwt/jwtauth/filter/CustomAuthenticationFilter.java
@@ -2,8 +2,10 @@ package com.authorization.jwt.jwtauth.filter;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.stream.Collectors;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -12,8 +14,10 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -52,6 +56,11 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
         String refreshToken = generateJTWToken(user, 24000, request.getRequestURL().toString(), algorithm);
         response.setHeader("access_token", accessToken);
         response.setHeader("refresh_token", refreshToken);
+        Map<String, String> tokens = new HashMap<>();
+        tokens.put("access_token", accessToken);
+        tokens.put("refresh_token", refreshToken);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        new ObjectMapper().writeValue(response.getOutputStream(), tokens);
     }
 
     private String generateJTWToken(User user, int milliseconds, String issuer, String claim, Algorithm algorithm){
@@ -75,5 +84,17 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
         return user.getAuthorities().stream()
             .map(role -> role.getAuthority())
             .collect(Collectors.toList());
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+            AuthenticationException failed) throws IOException, ServletException {
+        String username = request.getParameter("username");
+        String message = failed.getMessage();
+        Map<String, String> unsuccesfulMsg = new HashMap<>();
+        unsuccesfulMsg.put("username", username);
+        unsuccesfulMsg.put("message", message);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        new ObjectMapper().writeValue(response.getOutputStream(), unsuccesfulMsg);
     }
 }
