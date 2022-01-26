4190e33aaa11624816fa41d77acaa60f1da6728d
Ricardo Serrano <ricardo.ss@live.com>
2022-01-26 14:14:44 -0600

    feat: Add AuthorizationFIlter

--- src/main/java/com/authorization/jwt/jwtauth/filter/CustomAuthorizationFilter.java
+++ src/main/java/com/authorization/jwt/jwtauth/filter/CustomAuthorizationFilter.java
@@ -1,22 +1,84 @@
 package com.authorization.jwt.jwtauth.filter;
 
 import java.io.IOException;
+import java.net.http.HttpRequest;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class CustomAuthorizationFilter extends OncePerRequestFilter{
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         
-        if(request.getServletPath().equals("/login")) filterChain.doFilter(request, response);
+        if(request.getServletPath().equals("/login")){
+            filterChain.doFilter(request, response);
+            return;
+        } 
+            
         
+        String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        
+        if(!(authorizationHeader != null && authorizationHeader.startsWith("Bearer "))){
+            filterChain.doFilter(request, response);
+            return;
+        } 
+
+        try{
+            String token = authorizationHeader.substring("Bearer ".length());
+            Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+            JWTVerifier verifier = JWT.require(algorithm).build();
+            DecodedJWT decodedJWT = verifier.verify(token);
+                
+            String username = decodedJWT.getSubject();
+            String[] roles = decodedJWT.getClaim("roles").asArray(String.class);
+            Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+                
+            Stream.of(roles).forEach(role -> {
+                authorities.add(new SimpleGrantedAuthority(role));
+            });
+
+            UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(username, null, authorities);
+
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                
+            filterChain.doFilter(request, response);
+           
+            }catch(Exception ex){
+                log.error("Error logging in", ex.getMessage());
+                response.setHeader("error", ex.getMessage());
+                response.setStatus(HttpStatus.FORBIDDEN.value());
+                Map<String, String> error = new HashMap<>();
+                error.put("error_message", ex.getMessage());
+                response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                new ObjectMapper().writeValue(response.getOutputStream(), error);
+            }
     }
     
 }
